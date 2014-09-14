7b7a77311a52f10df6e9d9f68438377c83df6515
Andrew Arnott <andarno@microsoft.com>
2014-09-14 16:45:08 -0700

    Add prototype simple API.

---
+++ DiffPlex/Simple.cs
@@ -0,0 +1,36 @@
+﻿namespace DiffPlex
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Simple
+    {
+        private static readonly Differ DifferInstance = new Differ();
+        private static readonly InlineDiffBuilder InlineDiffBuilderInstance = new InlineDiffBuilder(DifferInstance);
+        private static readonly SideBySideDiffBuilder SideBySideDiffBuilderInstance = new SideBySideDiffBuilder(DifferInstance);
+
+        [Flags]
+        public enum Options
+        {
+            None,
+            IgnoreCase,
+            IgnoreWhitespace,
+        }
+
+        public static DiffPaneModel CompareInline(string before, string after, Options options = Options.None)
+        {
+            // TODO: do something with options.
+            // The currently exposed API in other classes makes this difficult or impossible.
+            return InlineDiffBuilderInstance.BuildDiffModel(before, after);
+        }
+
+        public static SideBySideDiffModel CompareSideBySide(string before, string after, Options options = Options.None)
+        {
+            // TODO: do something with options.
+            // The currently exposed API in other classes makes this difficult or impossible.
+            return SideBySideDiffBuilderInstance.BuildDiffModel(before, after);
+        }
+    }
+}
